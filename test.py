d3b0f6a1ea1520d86c1d6cb6e258c2b6f9a14036
sonali <96821408+patidar-sonali@users.noreply.github.com>
2022-07-23 23:17:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+from cv2 import VideoCapture
+import requests
+import cv2
+import numpy as np
+
+url = "http://192.168.0.6:8080/shot.jpg"
+
+while True:
+    cam = VideoCapture(0)
+    imgNp = np.array(bytearray(cam.content), dtype=np.uint8)
+    img = cv2.imdecode(imgNp, -1)
+    cv2.imshow("cam", img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
